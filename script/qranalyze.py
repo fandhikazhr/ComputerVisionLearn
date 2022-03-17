0a7392d4c20d16ba0faa5e6f8c5678434ec6497b
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-03-18 01:28:02 +0700

    Update qranalyze.py

--- script/qranalyze.py
+++ script/qranalyze.py
@@ -7,3 +7,7 @@ lower_white = np.array([40,0,220], np.uint8)
 upper_white = np.array([172,255,255], np.uint8)
 lower_black = np.uint8([5,5,5])
 upper_black = np.uint8([0,0,0])
+
+cap = cv2.VideoCapture(0)
+cap.set(3,640)
+cap.set(4,480)

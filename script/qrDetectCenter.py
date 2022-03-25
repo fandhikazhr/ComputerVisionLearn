97435409ead780090aec6aa33b4b78a4e89cd6fd
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-03-25 10:39:56 +0700

    Update qrDetectCenter.py

--- script/qrDetectCenter.py
+++ script/qrDetectCenter.py
@@ -2,3 +2,10 @@ import cv2
 import numpy as np
 import imutils
 from pyzbar.pyzbar import decode
+
+lower_white = np.array([0,0,168], np.uint8)
+upper_white = np.array([172,111,255], np.uint8)
+detect = False
+cap = cv2.VideoCapture(0)
+cap.set(3,640)
+cap.set(4,480)

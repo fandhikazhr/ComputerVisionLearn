6b6e7f24504dcaabd6df94df59bc9bf225e605f2
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-03-25 10:40:22 +0700

    Update qrDetectCenter.py

--- script/qrDetectCenter.py
+++ script/qrDetectCenter.py
@@ -9,3 +9,13 @@ detect = False
 cap = cv2.VideoCapture(0)
 cap.set(3,640)
 cap.set(4,480)
+
+while True:
+    success, img = cap.read()
+    for barcode in decode(img):
+        myData = barcode.data.decode('utf-8')
+        pts = np.array([barcode.polygon],np.int32)
+        pts = pts.reshape((-1,1,2))
+        cv2.polylines(img,[pts],True,(80,255,0),5)
+        pts2 = barcode.rect
+        detect = True

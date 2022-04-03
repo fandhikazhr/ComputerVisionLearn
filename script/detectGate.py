679462426397fba9a8f5b839658c3a55f2971170
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-04-03 15:49:46 +0700

    Update detectGate.py

--- script/detectGate.py
+++ script/detectGate.py
@@ -7,3 +7,12 @@ def nothing(x):
 
 cap = cv2.VideoCapture(0)
 settinghsv=True
+if settinghsv:
+    cv2.namedWindow("Tracking")
+    cv2.createTrackbar("LH", "Tracking", 0, 255, nothing)
+    cv2.createTrackbar("LS", "Tracking", 70, 255, nothing)
+    cv2.createTrackbar("LV", "Tracking", 50, 255, nothing)
+    cv2.createTrackbar("UH", "Tracking", 10, 255, nothing)
+    cv2.createTrackbar("US", "Tracking", 255, 255, nothing)
+    cv2.createTrackbar("UV", "Tracking", 255, 255, nothing)
+print ("QR Detection Starting...")

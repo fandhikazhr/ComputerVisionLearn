7c48b49f5e444cd3fd52f7a23d3f3ccc72f45a3e
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-04-03 15:50:08 +0700

    Update detectGate.py

--- script/detectGate.py
+++ script/detectGate.py
@@ -16,3 +16,23 @@ if settinghsv:
     cv2.createTrackbar("US", "Tracking", 255, 255, nothing)
     cv2.createTrackbar("UV", "Tracking", 255, 255, nothing)
 print ("QR Detection Starting...")
+while (1):
+    #frame = cv2.imread('smarties.png')
+    _, frame = cap.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    if settinghsv:
+        l_h = cv2.getTrackbarPos("LH", "Tracking")
+        l_s = cv2.getTrackbarPos("LS", "Tracking")
+        l_v = cv2.getTrackbarPos("LV", "Tracking")
+
+        u_h = cv2.getTrackbarPos("UH", "Tracking")
+        u_s = cv2.getTrackbarPos("US", "Tracking")
+        u_v = cv2.getTrackbarPos("UV", "Tracking")
+
+        lower_red = np.array([l_h, l_s, l_v])
+        upper_red = np.array([u_h, u_s, u_v])
+
+    else:
+        lower_red = np.array([0, 70, 50], np.uint8)
+        upper_red = np.array([10, 255, 255], np.uint8)
+        red = cv2.inRange(hsv, lower_red, upper_red)

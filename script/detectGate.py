dc8f69c9ffe69d290591b98738e8797bdab03ba6
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-04-03 15:50:51 +0700

    Update detectGate.py

--- script/detectGate.py
+++ script/detectGate.py
@@ -36,3 +36,7 @@ while (1):
         lower_red = np.array([0, 70, 50], np.uint8)
         upper_red = np.array([10, 255, 255], np.uint8)
         red = cv2.inRange(hsv, lower_red, upper_red)
+
+    mask = cv2.inRange(hsv, lower_red, upper_red)
+    cnts = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
